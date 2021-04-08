3de1913759c379c85cc0befca4130b5dafd2b9bf
Michael <mjsmyth1994@gmail.com>
2021-04-08 19:41:45 +0100

    Added support for gform_post_render

--- resources/assets/js/plugin.js
+++ resources/assets/js/plugin.js
@@ -35,150 +35,12 @@ const dollar = selector => $(`${selector.substr(0, 1)}${PREFIX}-${selector.subst
  * Document initialisation
  */
 $(function () {
-    const $fields = $('.ginput_container_postcode-lookup');
+    bindEvents();
 
-    if ($fields.length) {
-        $fields.each((_index, field) => {
-            const $trigger = $(field).find('.gfpcl-lookup');
-            const $searchWrap = $(field).find('.gfpcl-initial');
-            const $postcodeInput = $searchWrap.find($trigger.data('postcode-input'));
-            const $resultDropdown = $searchWrap.find('.lookup-results');
-            const $resultList = $resultDropdown.children('.result-list');
-            const $addressFields = $(field).find('.gfpcl-address-fields');
-            const $formToggle = $(field).find('.gfpcl-form-toggle > a');
-            const $fillableField = $(field).find('.gfpcl-fillable');
+    $(document).on('gform_post_render', () => {
+        bindEvents();
+    });
 
-            let limit = false;
-
-            if ($trigger.length) {
-                $trigger.on('click', function (event) {
-                    event.preventDefault();
-
-                    if (limit) {
-                        return;
-                    }
-
-                    limit = true;
-
-                    $searchWrap.addClass('loading');
-                    $resultList.empty();
-
-                    try {
-                        let userPostcode = validatePostcode($postcodeInput.val());
-                        let request = $.ajax({
-                            url: AJAX_URL,
-                            method: 'POST',
-                            data: {
-                                action: 'gf-postcode-lookup',
-                                postcode: userPostcode
-                            },
-                            success: response => {
-                                $searchWrap.removeClass('loading');
-                            
-                                if (response.status === 200 && response.data.length > 0) {
-                                    sortAddresses(response.data, 'line_1');
-
-                                    for (let i in response.data) {
-                                        let address = Object.filter(response.data[i], line => line.trim() !== '');
-
-                                        let subLines = [];
-                                        let $el = $('<li />', {
-                                            class: 'result',
-                                            html: `<p class="first-line">${address.line_1}</p>`
-                                        });
-
-                                        $el.data('postcode', userPostcode);
-
-                                        for (let key in address) {
-                                            let value = address[key];
-
-                                            $el.data(key, value);
-
-                                            if (key !== 'line_1') {
-                                                subLines.push(`<span class="${key}">${value}</span>`);
-                                            }
-                                        }
-
-                                        if (subLines.length) {
-                                            $el.append(`<small class="sub-lines">${subLines.join(', ')}</small>`);
-                                        }
-
-                                        $el.appendTo($resultList);
-                                    }
-
-                                    $resultDropdown.show();
-                                    $searchWrap.addClass('results-visible');
-                                } else {
-                                    // @TODO we need proper handling for the API status
-                                    throw new PostcodeException("We couldn't find any addresses matching the given postcode");
-                                }
-                            },
-                            fail: (error, message) => {
-                                throw new AjaxException(error);
-                            }
-                        }).always(() => {
-                            setTimeout(() => {
-                                limit = false;
-                            }, 1000);
-                        });
-                    } catch (e) {
-                        $searchWrap.removeClass('loading');
-
-                        Notifier(e.message);
-
-                        setTimeout(() => {
-                            limit = false;
-                        }, 1000);
-                    }
-                });
-
-                $postcodeInput.on('keyup keydown', function (event) {
-                    if (event.keyCode === 13) {
-                        event.stopPropagation();
-                        event.preventDefault();
-
-                        $trigger.trigger('click');
-                    }
-                });
-
-                $formToggle.on('click', function (event) {
-                    event.preventDefault();
-
-                    let $toggle = $(this);
-
-                    if ($toggle.hasClass('manual')) {
-                        $toggle.removeClass('manual').text($toggle.data('default'));
-                    } else {
-                        $toggle.addClass('manual').text($toggle.data('manual'));
-                    }
-
-                    $searchWrap.stop().slideToggle();
-                    $addressFields.stop().slideToggle();
-                });
-
-                /**
-                 * Concatenate address fields if changed manually to re-populate the fillable field
-                 */
-                let $addressFieldInputs = $addressFields.find('input');
-
-                $addressFieldInputs.on('keyup', function () {
-                    let addressComponents = {};
-
-                    $addressFieldInputs.each(function () {
-                        let $input = $(this);
-
-                        addressComponents[$input.data('line')] = $input.val().trim();
-                    });
-
-                    if (Object.keys(addressComponents).length) {
-                        $fillableField.val(JSON.stringify(addressComponents));
-                    }
-                });
-            }
-        });
-    }
-
-    // update
     $(document).on('click', '.result-list .result', function () {
         let $result = $(this);
         let lines = $result.data();
@@ -218,6 +80,153 @@ $(function () {
         }
     });
 });
+
+const bindEvents = () => {
+    const $fields = $(document).find('.ginput_container_postcode-lookup');
+
+    if (!$fields.length) {
+        return;
+    }
+
+    $fields.each((_index, field) => {
+        const $trigger = $(field).find('.gfpcl-lookup');
+        const $searchWrap = $(field).find('.gfpcl-initial');
+        const $postcodeInput = $searchWrap.find($trigger.data('postcode-input'));
+        const $resultDropdown = $searchWrap.find('.lookup-results');
+        const $resultList = $resultDropdown.children('.result-list');
+        const $addressFields = $(field).find('.gfpcl-address-fields');
+        const $formToggle = $(field).find('.gfpcl-form-toggle > a');
+        const $fillableField = $(field).find('.gfpcl-fillable');
+
+        let limit = false;
+
+        if ($trigger.length) {
+            $trigger.on('click', function (event) {
+                event.preventDefault();
+
+                if (limit) {
+                    return;
+                }
+
+                limit = true;
+
+                $searchWrap.addClass('loading');
+                $resultList.empty();
+
+                try {
+                    let userPostcode = validatePostcode($postcodeInput.val());
+                    let request = $.ajax({
+                        url: AJAX_URL,
+                        method: 'POST',
+                        data: {
+                            action: 'gf-postcode-lookup',
+                            postcode: userPostcode
+                        },
+                        success: response => {
+                            $searchWrap.removeClass('loading');
+                        
+                            if (response.status === 200 && response.data.length > 0) {
+                                sortAddresses(response.data, 'line_1');
+
+                                for (let i in response.data) {
+                                    let address = Object.filter(response.data[i], line => line.trim() !== '');
+
+                                    let subLines = [];
+                                    let $el = $('<li />', {
+                                        class: 'result',
+                                        html: `<p class="first-line">${address.line_1}</p>`
+                                    });
+
+                                    $el.data('postcode', userPostcode);
+
+                                    for (let key in address) {
+                                        let value = address[key];
+
+                                        $el.data(key, value);
+
+                                        if (key !== 'line_1') {
+                                            subLines.push(`<span class="${key}">${value}</span>`);
+                                        }
+                                    }
+
+                                    if (subLines.length) {
+                                        $el.append(`<small class="sub-lines">${subLines.join(', ')}</small>`);
+                                    }
+
+                                    $el.appendTo($resultList);
+                                }
+
+                                $resultDropdown.show();
+                                $searchWrap.addClass('results-visible');
+                            } else {
+                                // @TODO we need proper handling for the API status
+                                throw new PostcodeException("We couldn't find any addresses matching the given postcode");
+                            }
+                        },
+                        fail: (error, message) => {
+                            throw new AjaxException(error);
+                        }
+                    }).always(() => {
+                        setTimeout(() => {
+                            limit = false;
+                        }, 1000);
+                    });
+                } catch (e) {
+                    $searchWrap.removeClass('loading');
+
+                    Notifier(e.message);
+
+                    setTimeout(() => {
+                        limit = false;
+                    }, 1000);
+                }
+            });
+
+            $postcodeInput.on('keyup keydown', function (event) {
+                if (event.keyCode === 13) {
+                    event.stopPropagation();
+                    event.preventDefault();
+
+                    $trigger.trigger('click');
+                }
+            });
+
+            $formToggle.on('click', function (event) {
+                event.preventDefault();
+
+                let $toggle = $(this);
+
+                if ($toggle.hasClass('manual')) {
+                    $toggle.removeClass('manual').text($toggle.data('default'));
+                } else {
+                    $toggle.addClass('manual').text($toggle.data('manual'));
+                }
+
+                $searchWrap.stop().slideToggle();
+                $addressFields.stop().slideToggle();
+            });
+
+            /**
+                * Concatenate address fields if changed manually to re-populate the fillable field
+                */
+            let $addressFieldInputs = $addressFields.find('input');
+
+            $addressFieldInputs.on('keyup', function () {
+                let addressComponents = {};
+
+                $addressFieldInputs.each(function () {
+                    let $input = $(this);
+
+                    addressComponents[$input.data('line')] = $input.val().trim();
+                });
+
+                if (Object.keys(addressComponents).length) {
+                    $fillableField.val(JSON.stringify(addressComponents));
+                }
+            });
+        }
+    });
+}
 
 /**
  * Response handler functions
